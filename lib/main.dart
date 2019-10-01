d1adfbf22c850cde5cbfa5663b0ca8566ca5952d
Varun Resh Maddali <mvresh@gmail.com>
2019-10-01 14:22:50 +0530

    Took very long for the assemble debug to run. layout.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,54 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' as prefix0;
 
 void main() {
+
+  Container container1 = Container(
+    width: 100,
+    height: 100,
+    color: Colors.yellow,
+
+  );
+
+  Container container2 = Container(
+    width: 100,
+    height: 100,
+    color: Colors.green,
+
+  );
+
+  List<Widget> containers = [container1, container2];
+
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(),
+        backgroundColor: Colors.purple,
+        body: SafeArea(
+            child: prefix0.Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget> [
+                Container(
+                  width: 100,
+                  height: double.infinity,
+                  color: Colors.red,
+                ),
+                SizedBox(width: 30),
+                Column(
+                  children: containers,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+
+                ),
+                SizedBox(width: 30),
+                Container(
+                  width: 100,
+                  height: double.infinity,
+                  color: Colors.blue,
+                ),
+              ]
+            )),
       ),
     ),
   );
